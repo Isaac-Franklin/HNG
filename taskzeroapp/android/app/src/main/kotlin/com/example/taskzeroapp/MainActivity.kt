ac6089cb14908779b7aae45049d2c2f840eabd64
Franklin Isaac <praisefranklin005@gmail.com>
2025-02-02 18:59:06 +0100

    stage one task initialization

--- taskzeroapp/android/app/src/main/kotlin/com/example/taskzeroapp/MainActivity.kt
+++ taskzeroapp/android/app/src/main/kotlin/com/example/taskzeroapp/MainActivity.kt
@@ -3,3 +3,36 @@ package com.example.taskzeroapp
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+
+
+
+
+
+
+import android.content.Intent
+import android.net.Uri
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "open.browser"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "openUrl") {
+                val url = call.argument<String>("url")
+                if (url != null) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    startActivity(intent)
+                    result.success(null)
+                } else {
+                    result.error("INVALID_URL", "URL is null", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
